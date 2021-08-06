3b338ea8e78cf518dd7623f6fd5398b558a63e0e
Nick Moriarty <njmoriarty@gmail.com>
2021-08-06 23:29:31 +0100

    Break actions into methods

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@
 
 require 'lib/cube_coord.rb'
 require 'lib/island_map.rb'
+require 'lib/profiler.rb'
 
 # Class to encapsulate the whole game state
 class Game
@@ -11,6 +12,10 @@ class Game
     @args = args
     @board = IslandMap.new(4)
     @actions = []
+
+    @profiler = {
+      island_update: Profiler.new("Island Update", 5)
+    }
   end
 
   def tick
@@ -21,42 +26,12 @@ class Game
   def inputs
     inp = @args.inputs
     if inp.mouse.click
-      if inp.mouse.button_left
-        c = CubeCoord.from_point(inp.mouse.position).round!
-        if @actions.length < NUM_ACTIONS && @board.key?(c.to_axial)
-          @actions << {
-            rectangle: rect_around(c.to_point, 3, 0, 192, 0),
-            coord: c
-          }
-        end
-
-        @status = c.to_s
-      end
-
-      if inp.mouse.button_right
-        @actions.each do |a|
-          c = a[:coord]
-          @board[c.to_axial][:tile] = :grass
-        end
-        @actions.clear
-      end
-
-      if inp.mouse.button_middle
-        @board.each do |_k, tc|
-          score = 0
-          @board.each_adjacent(tc[:coordinate]) do |ta|
-            score += 1 if ta[:tile] == :grass
-          end
-          
-          tc[:new_tile] = :grass if score > 1
-        end
-
-        @board.each do |_k, t|
-          if t.key? :new_tile
-            t[:tile] = t[:new_tile]
-            t.delete :new_tile
-          end
-        end
+      case
+      when inp.mouse.button_left then place_action(inp.mouse.position)
+      when inp.mouse.button_right then commit_action
+      when inp.mouse.button_middle
+        @profiler[:island_update].profile { update_board }
+        @status = @profiler[:island_update].report
       end
     end
   end
@@ -76,5 +51,43 @@ class Game
   def rect_around(pt, radius, *args)
     d = radius * 2 + 1
     [pt.x - radius, pt.y - radius, d, d, *args]
+  end
+
+  def place_action(point)
+    c = CubeCoord.from_point(point).round!
+    if @actions.length < NUM_ACTIONS && @board.key?(c.to_axial)
+      @actions << {
+        rectangle: rect_around(c.to_point, 3, 0, 192, 0),
+        coord: c
+      }
+    end
+
+    @status = c.to_s
+  end
+
+  def commit_action
+    @actions.each do |a|
+      c = a[:coord]
+      @board[c.to_axial][:tile] = :grass
+    end
+    @actions.clear
+  end
+
+  def update_board
+    @board.each do |_k, tc|
+      score = 0
+      @board.each_adjacent(tc[:coordinate]) do |ta|
+        score += 1 if ta[:tile] == :grass
+      end
+
+      tc[:new_tile] = :grass if score > 1
+    end
+
+    @board.each do |_k, t|
+      if t.key? :new_tile
+        t[:tile] = t[:new_tile]
+        t.delete :new_tile
+      end
+    end
   end
 end
