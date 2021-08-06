fe2a3fd0390ceeb8aa540c1a6b4543198213346e
Nick Moriarty <njmoriarty@gmail.com>
2021-08-06 16:44:42 +0100

    Basic grass placement

--- lib/game.rb
+++ lib/game.rb
@@ -4,9 +4,12 @@ require 'lib/cube_coord.rb'
 
 # Class to encapsulate the whole game state
 class Game
+  NUM_ACTIONS = 3
+
   def initialize(args)
     @args = args
     init_board(4)
+    @actions = []
   end
 
   def tick
@@ -16,12 +19,39 @@ class Game
 
   def inputs
     inp = @args.inputs
+    if inp.mouse.click
+      if inp.mouse.button_left
+        c = CubeCoord.from_point(inp.mouse.position).round!
+        if @actions.length < NUM_ACTIONS && @board_map.key?(c.to_axial)
+          @actions << {
+            rectangle: rect_around(c.to_point, 3, 0, 192, 0),
+            coord: c
+          }
+        end
+      end
+
+      if inp.mouse.button_right
+        @actions.each do |a|
+          c = a[:coord]
+          @board_map[c.to_axial] = {
+            solid: rect_around(c.to_point, CubeCoord::SIZE / 2 - 1, 0, 160, 0),
+            outline: rect_around(c.to_point, CubeCoord::SIZE / 2, 0, 120, 0),
+            tile: :grass
+          }
+        end
+        @actions.clear
+      end
+    end
   end
 
   def draw
     @board_map.values.each do |item|
       @args.outputs.solids << item[:solid]
       @args.outputs.borders << item[:outline]
+    end
+
+    @actions.each do |item|
+      @args.outputs.borders << item[:rectangle]
     end
   end
 
