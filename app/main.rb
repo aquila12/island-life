49fbaf473fc209a1ee3853acc9a5b973abbca5cc
Nick Moriarty <njmoriarty@gmail.com>
2021-08-06 16:19:42 +0100

    Add island creation (new)

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'lib/cube_coord.rb'
+require 'lib/game.rb'
 
 def tick(args)
-  $game = @game = CubeCoord.new(args) if args.tick_count.zero?
+  $game = @game = Game.new(args) if args.tick_count.zero?
   f = args.tick_count % 256
   @game.tick
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,12 @@
 # frozen_string_literal: true
 
-require 'lib/hex_tile_map.rb'
-require 'lib/hex_tile_map_view.rb'
+require 'lib/cube_coord.rb'
 
 # Class to encapsulate the whole game state
 class Game
-  COLOURS = {
-    blue: [64, 64, 192],
-    yellow: [255, 192, 0]
-  }
-
-  TILE_H = 30
-  TILE_W = (0.866 * TILE_H).round
-
   def initialize(args)
     @args = args
-    @map = HexTileMap.new(15, 15, shape: :long_odds, default: COLOURS[:blue])
-    @map_view = HexTileMapView.new(@map)
-    @map_view.x_scl = TILE_W
-    @map_view.y_scl = TILE_H
+    init_board(4)
   end
 
   def tick
@@ -28,26 +16,36 @@ class Game
 
   def inputs
     inp = @args.inputs
-    m = inp.mouse
-    if m.click
-      if m.button_left
-        p, q = @map_view.xy_to_pq(m.click.point)
-        @rect = [p * TILE_W - 2, q * TILE_H - 2, 5, 5, 192, 0, 0]
-        @map.set_tile(p, q, COLOURS[:yellow])
-      elsif m.button_right
-        p, q = @map_view.xy_to_pq(m.click.point, snap: :corner)
-        @rect = [p * TILE_W - 2, q * TILE_H - 2, 5, 5, 0, 192, 0]
-        [[p - 0.25, q - 0.25], [p + 0.25, q - 0.25], [p + 0.25, q + 0.25], [p - 0.25, q + 0.25]]
-        .each { |p, q| @map.set_tile(p, q, COLOURS[:blue]) }
-      end
-    end
   end
 
   def draw
-    @map_view.each_tile do |tile, coords|
-      @args.outputs.solids << [coords[:x], coords[:y], TILE_W, TILE_H, *tile]
-      @args.outputs.borders << [coords[:x], coords[:y], TILE_W + 1, TILE_H + 1, 160, 160, 160]
+    @board_map.values.each do |item|
+      @args.outputs.solids << item[:solid]
+      @args.outputs.borders << item[:outline]
     end
-    @rect && @args.outputs.borders << @rect
+  end
+
+  private
+
+  def rect_around(pt, radius, *args)
+    d = radius * 2 + 1
+    [pt.x - radius, pt.y - radius, d, d, *args]
+  end
+
+  def init_board(radius)
+    @board_map = {}
+    origin = CubeCoord.new(0, 0, 0)
+    (-4..4).each do |p|
+      (-4..4).each do |q|
+        c = CubeCoord.from_axial(p, q)
+        next if c.distance_from(origin) > radius
+
+        @board_map[c.to_axial] = {
+          solid: rect_around(c.to_point, CubeCoord::SIZE / 2 - 1, 160, 160, 160),
+          outline: rect_around(c.to_point, CubeCoord::SIZE / 2, 120, 120, 120),
+          tile: :waste
+        }
+      end
+    end
   end
 end
