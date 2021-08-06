d4f1e12133b0bfcd3710242e24d8fa8f37c2ae18
Nick Moriarty <njmoriarty@gmail.com>
2021-08-06 17:59:12 +0100

    Add coordinate utility methods
    Dagnabbit, no operator overloading in mruby :(

--- lib/cube_coord.rb
+++ lib/cube_coord.rb
@@ -13,6 +13,10 @@ class CubeCoord
     @x, @y, @z = x, y, z
   end
 
+  def self.[](*coords)
+    new(*coords)
+  end
+
   attr_reader :x, :y, :z
 
   def to_axial
@@ -23,8 +27,14 @@ class CubeCoord
     new(-(p + q), p, q)
   end
 
+  def to_point(origin = DEFAULT_ORIGIN)
+    p, q = to_axial
+    q_2 = q / 2.0
+    [origin.x + SIZE * (p + q_2),  origin.y - ROOT_3_SIZE * q_2]
+  end
+
   def self.from_point(pt, origin = DEFAULT_ORIGIN)
-    x, y = [pt.x - origin.x, pt.y - origin.y]
+    x, y = pt.x - origin.x, pt.y - origin.y
     y_r3s = y / ROOT_3_SIZE
     x_s = x / SIZE
     from_axial(x_s + y_r3s, -2 * y_r3s)
@@ -34,22 +44,20 @@ class CubeCoord
     "[#{@x}, #{@y}, #{@z}]"
   end
 
-  def to_point(origin = DEFAULT_ORIGIN)
-    p, q = to_axial
-    q_2 = q / 2.0
-    [origin.x + SIZE * (p + q_2),  origin.y - ROOT_3_SIZE * q_2]
+  def distance_from(other)
+    ((other.x - @x).abs + (other.y - @y).abs + (other.z - @z).abs) / 2
   end
 
   def round!
-    ix, iy, iz = [@x.round, @y.round, @z.round]
-    dx, dy, dz = [(ix - @x).abs, (iy - @y).abs, (iz - @z).abs]
+    ix, iy, iz = @x.round, @y.round, @z.round
+    dx, dy, dz = (ix - @x).abs, (iy - @y).abs, (iz - @z).abs
     m = [dx, dy, dz].max
     case m
     when dx then ix = -(iy + iz)
     when dy then iy = -(ix + iz)
     when dz then iz = -(iy + ix)
     end
-    @x, @y, @z = [ix, iy, iz]
+    @x, @y, @z = ix, iy, iz
     self
   end
 
@@ -57,49 +65,57 @@ class CubeCoord
     dup.round!
   end
 
-  def distance_from(other)
-    ((other.x - @x).abs + (other.y - @y).abs + (other.z - @z).abs) / 2
+  def add(other)
+    @x += other.x
+    @y += other.y
+    @z += other.z
+    self
+  end
+
+  def take(other)
+    @x -= other.x
+    @y -= other.y
+    @z -= other.z
+    self
+  end
+
+  def scale(scale)
+    @x *= scale
+    @y *= scale
+    @z *= scale
+    self
+  end
+
+  def plus(other)
+    dup.add other
+  end
+
+  def minus(other)
+    dup.take other
+  end
+
+  def times(scale)
+    dup.scale scale
+  end
+
+  ADJACENCY = [
+    new(1, -1, 0),
+    new(0, -1, 1),
+    new(-1, 0, 1),
+    new(-1, 1, 0),
+    new(0, 1, -1),
+    new(1, 0, -1)
+  ]
+
+  def ring(radius)
+    yield self if radius.zero?
+
+    coord = self.plus ADJACENCY[4].times(radius)
+    ADJACENCY.each do |v|
+      radius.times do
+        coord.add v
+        yield coord
+      end
+    end
   end
 end
-
-  # def initialize(args)
-  #   @args = args
-  #   @saved = []
-  # end
-  #
-  # def tick
-  #   inputs
-  #   draw
-  # end
-  #
-  # def inputs
-  #   inp = @args.inputs
-  #   m = inp.mouse
-  #   @coord = Coord.from_point(m.position)
-  #   @status = @coord.round.to_s
-  #   if m.click
-  #     @saved.clear if m.button_right
-  #
-  #     if m.button_left
-  #       @saved << @coord.round.to_point
-  #     end
-  #   end
-  # end
-  #
-  # def draw
-  #   if @coord
-  #     @args.outputs.borders << rect_around(@coord.to_point, 2, 160, 0, 0)
-  #     @args.outputs.borders << rect_around(@coord.round.to_point, 2, 0, 160, 0)
-  #   end
-  #
-  #   @saved.each do |pt|
-  #     @args.outputs.borders << rect_around(pt, Coord::SIZE / 2, 0, 0, 160)
-  #   end
-  #
-  #   @args.outputs.labels << [8, 720 - 8, @status] if @status
-  # end
-  #
-  # def rect_around(pt, radius, *args)
-  #   d = radius * 2 + 1
-  #   [pt.x - radius, pt.y - radius, d, d, *args]
-  # end
