771b747a0b74419bbf6fd9c2c082b4c2662cadad
Nick Moriarty <njmoriarty@gmail.com>
2021-08-09 22:57:50 +0100

    Move String#hexcolor to a separate module

--- lib/island_map.rb
+++ lib/island_map.rb
@@ -2,33 +2,9 @@
 
 # Class to encapsulate the whole game state
 class IslandMap < Hash
-  def self.colour(hexcolour)
-    hc = hexcolour.dup
-    hc.slice!(0) if hc.start_with? '#'
-
-    case hc.length
-    when 1, 3, 4
-      channels = hc.chars.map { |c| c.hex * 0x11 }
-    when 2, 6, 8
-      channels = []
-      until hc.empty? do
-        channels << hc.slice!(0,2).hex
-      end
-    else return [0, 0, 0, 255]
-    end
-
-    a = 255
-    case channels.length
-    when 1 then r = g = b = channels.first
-    when 3 then r, g, b = channels
-    when 4 then r, g, b, a = channels
-    end
-    [r, g, b, a]
-  end
-
   TILE = {
-    waste: { tint: colour('#9'), fill: [160, 160, 160], line: [120, 120, 120] },
-    grass: { tint: colour('#090'), fill: [0, 160, 0], line: [0, 120, 0] }
+    waste: { tint: '#9'.hexcolor, fill: [160, 160, 160], line: [120, 120, 120] },
+    grass: { tint: '#090'.hexcolor, fill: [0, 160, 0], line: [0, 120, 0] }
   }
 
   def initialize(radius)
---
+++ lib/string_hexcolor.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# Monkey patch on String to do extract hex colour as an array [r, g, b, a]
+class String
+  def hexcolor
+    return self[1..-1].hexcolor if start_with? '#'
+
+    case length
+    when 1 then c = Array.new(3, hex * 0x11)
+    when 2 then c = Array.new(3, hex)
+    when 3, 4 then c = chars.map { |c| c.hex * 0x11 }
+    when 6, 8
+      c = []
+      buf = dup
+      until buf.empty?
+        c << buf.slice!(0,2).hex
+      end
+    else
+      return [0, 0, 0, 255]
+    end
+    c.length < 4 ? c << 255 : c
+  end
+end
