e49ac76b2aa13ac6b2d8f73d68602f568d38f693
Nick Moriarty <njmoriarty@gmail.com>
2021-08-09 23:00:25 +0100

    Typo - terms

--- lib/profiler.rb
+++ lib/profiler.rb
@@ -1,6 +1,6 @@
 # Copyright 2020 Nick Moriarty
 #
-# This file is provided under the term of the Eclipse Public License, the full
+# This file is provided under the terms of the Eclipse Public License, the full
 # text of which can be found in EPL-2.0.txt in the licenses directory of this
 # repository.
 
