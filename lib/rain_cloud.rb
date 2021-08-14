62c887dfd4ee7d5fc7858b5ac298179a831618b2
John Moriarty <jojomot@gmail.com>
2021-08-14 21:35:13 +0100

    Add first bits of new actions

--- lib/rain_cloud.rb
+++ lib/rain_cloud.rb
@@ -32,3 +32,36 @@ class RainCloud
     )
   end
 end
+
+class RainCloud2
+  def initialize(coord)
+    @coord = coord
+    @x, @y = coord.to_point
+    @rain_y = 4.0
+  end
+
+  attr_reader :x, :y
+  attr_reader :coord
+
+  def animate
+    @rain_y -= 0.333
+    @rain_y += 11 if @rain_y < 4
+  end
+
+  def draw_override(canvas)
+    animate
+
+    draw_fragment canvas, @x - 3, @y + 2, 7, 4, 0, 0
+    draw_fragment canvas, @x - 3, @y - 1, 7, 3, 0, @rain_y.to_i
+  end
+
+  def draw_fragment(canvas, x, y, w, h, tx, ty)
+    canvas.draw_sprite_3(
+      x, y, w, h, 'resources/raincloud2.png',
+      nil, nil, nil, nil, nil, # Angle A R G B
+      tx, ty, w, h, # Tile coords (top-down)
+      nil, nil, nil, nil, # Flip H V, Anchor X Y
+      nil, nil, nil, nil # Source coords (bottom-up)
+    )
+  end
+end
